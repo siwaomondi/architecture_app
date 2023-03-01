c0f21907984917f765ca67bd44410d0ea56ed34a
Smtp272 <101472734+Smtp272@users.noreply.github.com>
2023-03-01 23:06:41 +0300

    Add urls.py to django_server apps

--- backend/django_server/django_server/urls.py
+++ backend/django_server/django_server/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("survey/",include("app_survey.urls")),
+    path("rent_buddy/",include("app_housing.urls")),
+    path("architecture/",include("app_architecture.urls")),
+    
 ]
